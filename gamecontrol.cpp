bce07e669d3a0b490a0bcaaaf6cbc2cfd4e3d6f1
Arturo Curiel <curieldaz@gmail.com>
2016-02-04 22:59:08 -0600

    Removed manual loop; animation handled by the Animate Framework

--- gamecontrol.cpp
+++ gamecontrol.cpp
@@ -3,19 +3,34 @@
 
 GameControl::GameControl(){
 
+    // Game is running
     running = true;
 
+    // Creates a new scene
     scene = new GameScene();
-    handler = new EventHandler();
     if(scene != NULL){
+
+        // Adds the created scene to a view
         view = new GameView(this->scene);
         if(view != NULL){
             view->show();
+
+            // Everything looks good,
+            // start event handling
+            handler = new EventHandler();
         }
     }
+
+    // Maximize view's associated widget
     view->setWindowState(Qt::WindowMaximized);
+
+    // Create a demo player
+    // We pass the window's size to know
+    // where its borders are
     Agent *player = new Agent(QSize(view->width(),
                                     view->height()));
+
+    // Add demo player to the scene
     scene->addItem(player);
     player->goUp();
 }
--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@ int main(int argc, char **argv)
     QApplication app(argc, argv);
     GameControl game;
 
+    /*// Manual game loop
     while(game.isRunning()){
         app.processEvents();
 
         // 24pfs
         usleep(40000);
     }
+    return 0;*/
+    return app.exec();
 
-    return 0;
 }
